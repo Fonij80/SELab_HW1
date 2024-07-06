aed26c4f102165a02e58b75abe19485bc31d6d7f
Anita <anitaalikhani12@gmail.com>
2024-07-06 14:49:56 +0330

    Added Posts component to render all posts

---
+++ blog/src/components/Posts.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Post1 from "./Post1";
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+
+const Posts = () => {
+	return (
+		<Container>
+			<Row className="justify-content-between">
+				<Col md={8} className="mb-4 mt-4">
+					<Post1 />
+				</Col>
+				<Col md={2} className="mt-4 float-right">
+					<Card>
+						<Card.Body>
+							<Card.Title>Recent Posts</Card.Title>
+							<ul className="list-unstyled">
+								<li><a href="#">JavaScript</a></li>
+							</ul>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+export default Posts;
