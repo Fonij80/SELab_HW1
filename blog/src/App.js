cdcb5ff32661b81d4987110ea54a112b33936927
Reyhaneh <r.akhlaghian@gmail.com>
2024-07-07 13:47:50 +0330

    Add simple footer component

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Posts from "./components/Posts";
 import Navbar from "./components/BlogNav"
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="main-container" style={{backgroundColor: "aliceblue"}}>
         <Navbar />
         <Posts />
+        <Footer />
     </div>
   );
 }
